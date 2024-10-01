dd32fee2e7c8a5c500c8ab3639f4e18a0afa95b2
Praneeth <boillapraneeth10@gmail.com>
2024-10-01 12:47:21 +0530

    Initial commit

---
+++ api/routes/orders.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const Order = require('../models/order');
+const Product = require('../models/product');
+
+router.get('/', (req, res, next) => {
+    Order.find()
+    .select('product quantity _id')
+    .populate('product', 'name')
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc =>{
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.post('/', (req, res, next) => {
+    Product.findById(req.body.productId)
+    .then(product => {
+        if(!product){
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+        const order = new Order({
+            _id: new mongoose.Types.ObjectId(),
+            product: req.body.productId,
+            quantity: req.body.quantity
+        });
+        return order.save();
+    })
+    .then(result => {
+        res.status(201).json({
+            message: 'Created Order successfully',
+            createdOrder: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + result._id
+                }
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    Order.findById(req.params.orderId)
+    .populate('product')
+    .then(order => {
+        if (!order) {
+            res.status(404).json({
+                message: "Order not found"
+            })
+        }
+        res.status(200).json({
+            order: order,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders'
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    Order.findByIdAndDelete(req.params.orderId)
+    .then(result => {
+        console.log("Delete Order result", result);
+        res.status(200).json({
+            message: 'Order deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/orders',
+                body: {productId: 'ID', quantity: 'Number'}
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    })
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productsRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://boillapraneeth10:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop.i8yle.mongodb.net/?retryWrites=true&w=majority&appName=node-rest-shop',
+    { ssl: true }
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Method", 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/products', productsRoutes);
+app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+});
+
+module.exports = app;
+
+
+// mongodb+srv://boillapraneeth10:ir5V00HRXJQCbGXs@node-rest-shop.i8yle.mongodb.net/?retryWrites=true&w=majority&appName=node-rest-shop
